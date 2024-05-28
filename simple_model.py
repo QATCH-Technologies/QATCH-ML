7fd8c8ca102d76fbe529628538a750478cafae87
Paul MacNichol <32798037+PaulMacNichol@users.noreply.github.com>
2024-05-28 14:02:00 -0500

    Update simple_model.py
    New model architecture and simplified data vectors.

--- simple_model.py
+++ simple_model.py
@@ -1,3 +1,27 @@
+"""
+QATCH Model Training Script
+
+This script contains functions and procedures for training a convolutional neural network model
+to predict points of interest (POIs) in time series data.
+
+Author: Paul MacNichol
+Date: 05-28-2024
+
+Requirements:
+- pandas
+- numpy
+- scikit-learn
+- tensorflow
+- utils (custom module)
+
+Usage:
+- Ensure the necessary packages are installed.
+- Place raw data CSV files and their corresponding POI CSV files in the specified directory.
+- Adjust parameters such as file paths, features, and model architecture as needed.
+- Run the script to train the model and save it for future use.
+"""
+
+import msvcrt
 import pandas as pd
 import numpy as np
 import os
@@ -5,7 +29,9 @@ import csv
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
+from scipy.signal import savgol_filter
+
+from tensorflow.keras.layers import Dense
 from utils import status, error, info, linebreak, loading, echo, plot_loss
 
 """ Ensures pandas displays all rows of a dataframe on print(). """
@@ -18,18 +44,51 @@ MODEL_PATH = "SavedModel/QATCH-simple-v1"
 CONTENT_DIRECTORY = "content/training_data_with_points"
 
 """ Set of features to not include in training. """
-DROPPED_FEATURES = ["Date", "Time", "Ambient", "Temperature", "POIs"]
-
-""" Set of features to include in training. """
-TRAINING_FEATURES = [
+DROPPED_FEATURES = [
+    "Date",
+    "Time",
+    "Ambient",
+    "Temperature",
+    "POIs",
     "Relative_time",
     "Peak Magnitude (RAW)",
-    "Resonance_frequency",
+    "Resonance_Frequency",
     "Dissipation",
 ]
 
+""" Set of features to include in training. """
+TRAINING_FEATURES = ["der2"]
+
 """ Target feature to train. """
 TARGET_FEATURES = ["POIs"]
+
+
+def compute_second_derivative(file_path, window_size=30, polyorder=3):
+    """
+    Compute the second derivative of a data array and apply smoothing.
+
+    Parameters:
+        data (array-like): Input data array.
+        window_size (int): Window size for Savitzky-Golay filter. Should be odd.
+        polyorder (int): Polynomial order for Savitzky-Golay filter.
+
+    Returns:
+        array: Smoothed second derivative of the input data.
+    """
+    data = pd.read_csv(file_path)
+
+    # Extract the 'Dissipation' column into a list
+    data = np.array(data["Dissipation"])
+
+    # Compute the second derivative using central finite differences
+    data[0] = data[-1] = 0
+    dx = data[1] - data[0]
+    d2_data = np.gradient(np.gradient(data, dx), dx)
+
+    # Apply Savitzky-Golay smoothing
+    smoothed_d2_data = savgol_filter(d2_data, window_size, polyorder)
+
+    return smoothed_d2_data
 
 
 def read_data(raw_data_file, poi_file):
@@ -46,7 +105,7 @@ def read_data(raw_data_file, poi_file):
         DataFrame: The combined DataFrame containing binary indicators for POIs.
     """
     raw_data = pd.read_csv(raw_data_file)
-
+    der2 = compute_second_derivative(raw_data_file)
     pois_flags, pois_indexes = [], []
     with open(poi_file, "r") as csvfile:
         csvreader = csv.reader(csvfile)
@@ -60,17 +119,18 @@ def read_data(raw_data_file, poi_file):
         else:
             pois_flags.append(0)
     raw_data["POIs"] = pois_flags
+    raw_data["der2"] = der2
     return raw_data
 
 
 def load_data_from_directory(data_directory):
-    """Opens the training data directory.  This function controls execution flow of read_data().
+    """Load data from a directory containing Point of Interest (POI) files.
 
     Args:
-        data_directory (str): Path to the directory containing training data.
+        data_directory (str): Path to the directory containing POI files.
 
     Returns:
-        list: Returns a list of combined DataFrames
+        list: A list of DataFrames containing combined data from POI and raw files.
     """
     dfs = []
 
@@ -87,15 +147,14 @@ def load_data_from_directory(data_directory):
 
 
 def extract_features(data_frames):
-    """This function is used for feature extraction.  The function extracts the target 'POIs'
-    data from each DataFrame in the input (y) and drops the untrained features from each saving
-    saving the remaining data as X.  The function returns a list of correlated tuples (X, y).
+    """
+    Extracts features from a list of data frames.
 
     Args:
-        data_frames (list): List of DataFrames.
+        data_frames (list): A list containing pandas DataFrames, each representing a dataset.
 
     Returns:
-        list: Returns a list of tuples containing training and target features.
+        list: A list of tuples containing the feature matrix (X) and target vector (y) for each DataFrame.
     """
     corr_data = []
     for df in data_frames:
@@ -107,6 +166,16 @@ def extract_features(data_frames):
 
 
 def normalize_features(features):
+    """Normalize features using StandardScaler.
+
+    Args:
+        features (list of tuples): A list containing tuples where each tuple contains
+            the raw features (X_raw) and corresponding labels (y_raw).
+
+    Returns:
+        list of tuples: A list of tuples where each tuple contains the normalized
+            features (X_norm) and corresponding labels (y_raw).
+    """
     standard_scaler = StandardScaler()
     features_norm = []
     for item in features:
@@ -120,6 +189,18 @@ def normalize_features(features):
 
 
 def reshape_dataframe_tuples(tuple_list):
+    """Reshape DataFrame tuples to have consistent dimensions.
+
+    Args:
+        tuple_list (list of tuples): A list containing tuples where each tuple
+            contains a DataFrame for features (X_df) and another DataFrame for
+            labels (y_df).
+
+    Returns:
+        tuple: A tuple containing two lists. The first list contains DataFrames
+            with standardized dimensions for features, and the second list
+            contains DataFrames with standardized dimensions for labels.
+    """
     max_rows_X = max(df[0].shape[0] for df in tuple_list)
     max_cols_X = max(df[0].shape[1] for df in tuple_list)
 
@@ -146,28 +227,41 @@ def reshape_dataframe_tuples(tuple_list):
 
 
 def build_model():
+    input_shape = (55340, 1)
     model = Sequential(
         [
-            Dense(64, activation="relu"),
-            Dropout(0.2),
+            Dense(64, activation="relu", input_shape=input_shape),
             Dense(32, activation="relu"),
-            Dropout(0.2),
             Dense(1, activation="sigmoid"),
         ]
     )
-    model.compile(optimizer="adam", loss="mean_absolute_error", metrics=["accuracy"])
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+    model.summary()
     return model
 
 
 def train_model(X_train, y_train, X_val, y_val):
+    """
+    Trains a neural network model using the provided training data and evaluates it on the validation data.
+
+    Args:
+        X_train (numpy.ndarray): Input features for training.
+        y_train (numpy.ndarray): Target labels for training.
+        X_val (numpy.ndarray): Input features for validation.
+        y_val (numpy.ndarray): Target labels for validation.
+
+    Returns:
+        tuple: A tuple containing the trained model and the training history.
+               - The trained model (tensorflow.python.keras.engine.sequential.Sequential): The trained neural network model.
+               - The training history (dict): A dictionary containing training/validation loss and metrics per epoch.
+    """
     model = build_model()
     X_train = np.asarray(X_train).astype(np.float32)
     y_train = np.asarray(y_train).astype(np.float32)
     X_val = np.asarray(X_val).astype(np.float32)
     y_val = np.asarray(y_val).astype(np.float32)
-
     history = model.fit(
-        X_train, y_train, epochs=20, batch_size=32, validation_data=(X_val, y_val)
+        X_train, y_train, epochs=5, batch_size=32, validation_data=(X_val, y_val)
     )
     return model, history
 
@@ -194,16 +288,10 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(
         X_standard, y_standard, test_size=0.2, random_state=42
     )
-    # print(X_train)
-    # input("Press any key to continue...")
-    # print(y_train)
-    # input("Press any key to continue...")
-    # print(X_test)
-    # input("Press any key to continue...")
-    # print(y_test)
-    # input("Press any key to continue...")
-
     model, history = train_model(X_train, y_train, X_test, y_test)
     status(f"{model} training complete with no errors.")
     plot_loss(history)
     model.save(MODEL_PATH)
+    info(f"Model saved to path '{MODEL_PATH}'")
+    print("Press any key to exit...")
+    msvcrt.getch()
