00c687e3154b93718aeedf57a22c84d62598519d
Alexander J. Ross <alexander.ross@qatchtech.com>
2024-06-04 15:54:38 -0400

    Better ability to pull training data from Production Data folder in Dropbox. Modify paths to 'production_data_path' and 'training_data_path' before running. Do not point training data to the same (or a nested) folder as this will result in an infinite job.

--- pull_training_data_from_production.py
+++ pull_training_data_from_production.py
@@ -1,24 +1,64 @@
 import os
 import shutil
+import numpy as np
 
 roots = []
 paths = []
 files = []
 
+num_files_per_run = 3
+
 def iterate_files(directory):
     for root, dirs, files in os.walk(directory):
+        file_poi = None
+        file_xml = None
+        file_zip = None
         for file in files:
             # print(os.path.join(root, file))
-            if file.endswith("_poi.csv") or file == "capture.zip":
-                store_data_file(root, file)
+            if file.endswith("_poi.csv"):
+                file_poi = file
+            if file.endswith(".xml"):
+                with open(os.path.join(root, file), 'r') as f:
+                    xml_text = "\n".join(f.readlines()).upper()
+                    if "MM240506" in xml_text:
+                        print(f"Skipping {file}... bad batch 1")
+                        continue
+                    if "DD240501" in xml_text:
+                        print(f"Skipping {file}... bad batch 2")
+                        continue
+                file_xml = file
+            if file == "capture.zip":
+                file_zip = file
+        if all([file_poi, file_xml, file_zip]):
+            store_data_file(root, file_poi)
+            store_data_file(root, file_xml)
+            store_data_file(root, file_zip)
 
 def store_data_file(root, file):
-    roots.append(root)
-    paths.append(os.path.join(root, file))
+    i = int(len(roots) / num_files_per_run)
+    copy_from = os.path.join(root, file)
+    copy_to = os.path.join(training_data_path, f"{i:05.0f}")
+
+    try:
+        os.makedirs(copy_to, exist_ok=True)
+
+        root = copy_to
+        roots.append(root)
+
+        if os.path.isfile(os.path.join(copy_to, file)):
+            paths.append(os.path.join(copy_to, file))
+            raise FileExistsError()
+        
+        print(f"Copying {i:05.0f}:", os.path.basename(copy_from))
+        file = shutil.copy(copy_from, copy_to)
+        paths.append(file)
+
+    except:
+        print(f"Skipping {i:05.0f}/{file}... file already exists!")
 
 def process_stored_files():
     for x,y in enumerate(roots):
-        if roots.count(y) == 2:
+        if roots.count(y) == num_files_per_run:
             # print(paths[x])
             files.append(paths[x])
     skip_next = False
@@ -30,8 +70,8 @@ def process_stored_files():
             # check if already unzipped
             unzip_required = True
             unzipped_data_path = f
-            unzip_loc = os.path.join(os.path.dirname(f), "capture")
-            if os.path.exists(unzip_loc):
+            unzip_loc = os.path.dirname(f) #os.path.join(os.path.dirname(f), "capture")
+            if os.path.exists(unzip_loc) and False: # disable for new code
                 zf = os.listdir(unzip_loc)
                 for _f in zf:
                     if _f.endswith(".csv") and _f.find("_lower.csv") == -1 and _f.find("_tec.csv") == -1:
@@ -52,16 +92,24 @@ def process_stored_files():
                         if _f.endswith(".csv") and _f.find("_lower.csv") == -1 and _f.find("_tec.csv") == -1:
                             # unzip_required = False
                             unzipped_data_path = os.path.join(unzip_loc, _f)
+                            os.remove(f) # delete ZIP if data file found; otherwise leave capture.zip for review
                             break
             f = unzipped_data_path
-        src = f
-        dst = os.path.join(destination_path, os.path.basename(f))
-        if os.path.exists(src) and not os.path.exists(dst):
-            print("Copying:", os.path.basename(f))
-            shutil.copyfile(src, dst)
+        if False: # do not copy again, old code
+            src = f
+            dst = os.path.join(training_data_path, os.path.basename(f))
+            if os.path.exists(src) and not os.path.exists(dst):
+                print("Copying:", os.path.basename(f))
+                shutil.copyfile(src, dst)
+    for root in np.unique(roots): 
+        for file in os.listdir(root):
+            full_path = os.path.join(root, file)
+            if full_path.endswith('.crc') or full_path.endswith('_tec.csv'):
+                print("Purging extra files:", file)
+                os.remove(full_path) # delete extra files
 
-destination_path = r"C:\Users\Alexander J. Ross\Documents\QATCH Work\ML\content\testing-2024-02-27"
-iterate_files(r"C:\Users\Alexander J. Ross\Documents\QATCH Work\ML\content\testing-2024-02-27")
+production_data_path = r"C:\Users\Alexander J. Ross\Documents\QATCH Work\v2.6x branch\dev\logged_data"
+training_data_path = r"C:\Users\Alexander J. Ross\Documents\QATCH Work\ML\training_data"
+
+iterate_files(production_data_path)
 process_stored_files()
-
-
